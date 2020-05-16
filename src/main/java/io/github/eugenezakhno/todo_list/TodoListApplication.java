dc6c9e50e1b38aba427bd246bc0b2afba1b2d20d
EugeneZakhno <zohan1234567@gmail.com>
2020-05-17 00:39:10 +0300

    real first commit into todo_list

---
+++ src/main/java/io/github/eugenezakhno/todo_list/TodoListApplication.java
@@ -0,0 +1,23 @@
+package io.github.eugenezakhno.todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplateBuilder().build();
+    }
+
+
+
+}
